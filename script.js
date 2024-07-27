029ce0c5cea53bf63831a7d57fff15a7cc9551bd
Akshay-team <akshaykireet644@gmail.com>
2024-07-27 19:49:38 +0530

    new fearute

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-let a=document.querySelector("btn-submit");
-let b=document.querySelector(".itemsc");
-a.addEventListener("click",()=>{
-    let info=document.createElement("div");
-    info.setAttribute("class","infoitem");
-    console.log(info);
-    alert("infobox created")
-    let del=document.createElement("button");
-    del.setAttribute("class","delb");
-    let imgs=document.createElement("img");
-    imgs.setAttribute("class","delimg");
-    imgs.setAttribute("src","https://www.svgrepo.com/show/21045/delete-button.svg");
-    del.appendChild(imgs);
-    b.appendChild(info);
-    b.appendChild(del);   
+
+let a = document.querySelector(".submit");
+let b = document.querySelector(".items");
+a.addEventListener("click", () => {
+let text=document.getElementById("inputt").value;
+    alert("hello");
+    let c=document.createElement("div");
+    c.setAttribute("class","itemsc");
+    let info = document.createElement("div");
+    info.setAttribute("class", "infoitem");
+    info.innerHTML=text;
+    b.appendChild(info)
+    let delb=document.createElement("button");
+    delb.setAttribute("class","delb");
+    let im=document.createElement("img");
+    im.setAttribute("src","https://www.svgrepo.com/show/21045/delete-button.svg");
+    im.setAttribute("class","delimg")
+    delb.appendChild(im);
+    c.appendChild(info);
+    c.appendChild(delb);
+    b.appendChild(c);
+    
 })
